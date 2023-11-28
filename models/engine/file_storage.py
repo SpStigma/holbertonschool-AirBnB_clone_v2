e65e3200105fb9a32802ceffa2918b0968026c38
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-28 06:44:03 -0800

    Update FileStorage with Delete and CLS

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            return cls.__objects
+        else:
+            # If cls is not specified, return all objects from all classes
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +28,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is not None:
+            del FileStorage.__objects[obj.to_dict()['__class__'] + '.' + obj.id]
+            self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
