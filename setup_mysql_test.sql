a1e0e4a573fcf91c9af790e381e0564244ca534c
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-28 06:35:46 -0800

    Fix : confusion beteen two folders

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Prepare a MySQL statement for execution
 
-CREATE DATABASE IF not EXISTS hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF not EXISTS hbnb_test_db;
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
