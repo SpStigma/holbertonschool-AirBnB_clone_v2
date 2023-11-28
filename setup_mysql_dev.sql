48b5b95e0d90a5cb1e82e83474b251329f0aba94
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-28 06:24:56 -0800

    Fix Grant Select

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,4 +3,4 @@
 CREATE DATABASE IF not EXISTS hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
