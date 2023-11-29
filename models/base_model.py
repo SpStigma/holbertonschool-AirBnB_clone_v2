1a458151886f3e6f96a12a06d606acfb9cda9449
SpStigma <yoann.rivet0@gmail.com>
2023-11-29 14:48:50 +0100

    Correction of fefault for DateTime.

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,19 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, datetime.utcnow(), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -48,10 +50,11 @@ class BaseModel:
         dictionary['updated_at'] = self.updated_at.isoformat()
 
         if '_sa_instance_state' in dictionary.keys():
-            dictionary.pop('_sa_instance_state')
+            del dictionary['_sa_instance_state']
 
         return dictionary
 
     def delete(self):
+        """"""
         from models import storage
         storage.delete(self)
