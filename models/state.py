6b452677cf81a40dc790d095445a943d0aba8ded
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-29 06:33:29 -0800

    Getter attribute cities that returns list of City

--- models/state.py
+++ models/state.py
@@ -2,6 +2,8 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -9,3 +11,16 @@ class State(BaseModel, Base):
     
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities_relation = relationship('City', backref='state', cascade='all, delete')
+
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            city_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
