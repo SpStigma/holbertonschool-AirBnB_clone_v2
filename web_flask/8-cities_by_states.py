81c777094f9b738abf1cea515dd2369279cc9387
SpStigma <yoann.rivet0@gmail.com>
2023-12-12 12:28:53 +0100

    Fix: way of tchat

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Run this script to start the Flask web application on 0.0.0.0, port 5000."""
-
+"""Starts a Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
@@ -9,15 +8,19 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def list_cities_by_states():
-    states_list = storage.all(State)
-    return render_template("8-cities_by_states.html", states=states_list)
-
-
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with a list of all State objects in DBStorage."""
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+
+    return render_template("8-cities_by_states.html", states=sorted_states)
 
 
 if __name__ == "__main__":
