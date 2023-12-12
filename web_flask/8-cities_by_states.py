bc142683709538dd32c4b337c72952d7016124fb
SpStigma <yoann.rivet0@gmail.com>
2023-12-12 12:20:31 +0100

    Fix: change the code itself.

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
 """Run this script to start the Flask web application on 0.0.0.0, port 5000."""
+
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def list_cities_by_states():
+    states_list = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states_list)
+
+
 @app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy Session"""
+def teardown():
     storage.close()
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Display a HTML page with a list of all State objects in DBStorage."""
-    states = storage.all(State)
-    cities = storage.all(City)
-
-    return render_template("8-cities_by_states.html",
-                           cities=cities,
-                           states=states)
 
 
 if __name__ == "__main__":
