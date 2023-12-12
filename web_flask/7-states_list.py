6afd0d9b03bbb5bd9d05261b7c683b84e8aaf599
SpStigma <yoann.rivet0@gmail.com>
2023-12-12 10:56:37 +0100

    Fix: PEP8 validation.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,12 +8,14 @@ from models.city import City
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def display_html():
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
 
     return render_template("7-states_list.html", states=sorted_states)
+
 
 @app.teardown_appcontext
 def teardown(exception):
